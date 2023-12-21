e1d5791149bb13f23b1cfc23c2001fc5a1b6a69b
Anderson Sanches <andycds@gmail.com>
2023-12-21 20:03:29 -0300

    Continuação da classificação

--- classificacao/index.js
+++ classificacao/index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.post("/eventos", (req, res) => {
+const palavraChave = "importante";
 
+const funcoes = {
+    ObservacaoCriada: (observacao) => {
+        observacao.status = observacao.texto.includes(palavraChave) ? "importante" : "comum";
+        axios.post("http://localhost:1000/eventos", {
+            tipo: "ObservacaoClassificada",
+            dados: observacao
+        });
+    }
+};
+
+app.post("/eventos", (req, res) => {
+    funcoes[req.body.tipo](req.body.dados);
+    res.send({ msg: 'ok' });
 });
 
 app.listen(7000, () => console.log("Classificação. Porta 7000."));
